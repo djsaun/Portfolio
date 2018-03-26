27e5899a53f10588a78fb940fb3e4b939793f520
David Saunders <david@monkee-boy.com>
2018-03-26 08:51:44 -0500

    Moved GitHub activity code from Repos component into new Activity component

---
+++ src/components/Activity.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import moment from 'moment';
+
+const Activity = (props) => {
+  const { events } = props;
+  let last_year = "";
+
+  const github_activities = events.reduce((events, value, index) => {
+    value.created_at = moment(value.created_at).format('MMM DD, YYYY');
+
+    switch(value.type) {
+      case "WatchEvent":
+        value.action = value.payload.action.charAt(0).toUpperCase() + value.payload.action.slice(1) + " watching";
+        break;
+      case "PushEvent":
+        value.action = "Made a push event on";
+        break;
+      case "IssueCommentEvent":
+        value.action = value.payload.action.charAt(0).toUpperCase() + value.payload.action.slice(1) + " a comment on";
+        break;
+      case "ForkEvent":
+        value.action = "Made a clone of";
+        break;
+      case "PullRequestEvent":
+        value.action = value.payload.action.charAt(0).toUpperCase() + value.payload.action.slice(1) + " a pull request on";
+        break;
+      case "IssuesEvent":
+        value.action = value.payload.action.charAt(0).toUpperCase() + value.payload.action.slice(1) + " an issue on";
+        break;
+      case "CreateEvent":
+        value.action = "Created a " + value.payload.ref_type + " at";
+        break;
+      case "PullRequestReviewCommentEvent":
+        value.action = "Reviewed a pull request on";
+        break;
+      case "DeleteEvent":
+        value.action = "Deleted a " + value.payload.ref_type + " on";
+        break;
+      case "CommitCommentEvent":
+        value.action = "Made a comment on a commit in";
+        break;
+    }
+    if(value.created_at != last_year) {
+      var tempArr = [];
+      tempArr.push(value);
+      events.push({'year': value.created_at, 'data': tempArr});
+    } else {
+      events[events.length-1].data.push(value);
+    }
+    last_year = value.created_at;
+
+    return events;
+  }, [])
+
+  return(
+    <div>
+      {github_activities.map((event, i) => 
+       <div className="event" key={i}>
+         <h5>{event.year}</h5>
+         {event.data.map((action, i) => 
+           <p key={i}><a href={action.repo.url} target="_blank">{action.action} {action.repo.name}</a></p>
+         )}
+       </div>
+       )}
+    </div>
+  )
+}
+
+export default Activity;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,63 +1,13 @@
 import React from 'react';
 import Repo from './Repo';
-import moment from 'moment';
-
 import styles from '../styles/repos.module.css'
+import Activity from './Activity';
 
 class Repos extends React.Component {
 
   render() {
     const { repos } = this.props.repos;
     const { events } = this.props.repos;
-
-    let last_year = "";
-
-      const github_activities = events.reduce((events, value, index) => {
-        value.created_at = moment(value.created_at).format('MMM DD, YYYY');
-
-        switch(value.type) {
-          case "WatchEvent":
-            value.action = value.payload.action.charAt(0).toUpperCase() + value.payload.action.slice(1) + " watching";
-            break;
-          case "PushEvent":
-            value.action = "Made a push event on";
-            break;
-          case "IssueCommentEvent":
-            value.action = value.payload.action.charAt(0).toUpperCase() + value.payload.action.slice(1) + " a comment on";
-            break;
-          case "ForkEvent":
-            value.action = "Made a clone of";
-            break;
-          case "PullRequestEvent":
-            value.action = value.payload.action.charAt(0).toUpperCase() + value.payload.action.slice(1) + " a pull request on";
-            break;
-          case "IssuesEvent":
-            value.action = value.payload.action.charAt(0).toUpperCase() + value.payload.action.slice(1) + " an issue on";
-            break;
-          case "CreateEvent":
-            value.action = "Created a " + value.payload.ref_type + " at";
-            break;
-          case "PullRequestReviewCommentEvent":
-            value.action = "Reviewed a pull request on";
-            break;
-          case "DeleteEvent":
-            value.action = "Deleted a " + value.payload.ref_type + " on";
-            break;
-          case "CommitCommentEvent":
-            value.action = "Made a comment on a commit in";
-            break;
-        }
-        if(value.created_at != last_year) {
-          var tempArr = [];
-          tempArr.push(value);
-          events.push({'year': value.created_at, 'data': tempArr});
-        } else {
-          events[events.length-1].data.push(value);
-        }
-        last_year = value.created_at;
-
-        return events;
-      }, [])
     
     return(
       <div className={styles.repos}>
@@ -72,17 +22,7 @@ class Repos extends React.Component {
         </div>
         <div className="activity-feed">
           <h2 className={styles.header}>Recent Activity</h2>
-           <div>
-             {console.log(github_activities)}
-             {github_activities.map((event, i) => 
-              <div className="event" key={i}>
-                <h5>{event.year}</h5>
-                {event.data.map((action, i) => 
-                  <p key={i}><a href={action.repo.url} target="_blank">{action.action} {action.repo.name}</a></p>
-                )}
-              </div>
-              )}
-           </div>
+           <Activity events={events} />
         </div>
       </div>
     )
