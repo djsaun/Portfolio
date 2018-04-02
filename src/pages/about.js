d917caa9290227515fd4e678e52e7edc69f324a6
David Saunders <david@monkee-boy.com>
2018-04-02 15:26:07 -0500

    Updated about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,23 +5,13 @@ const About = (props) => {
   return(
     <div>
       <h1>About David</h1>
-      <p>I've been developing professionally for five years and I pride myself on creating  most recently as a Front End Web Developer at <a href="https://monkee-boy.com" target="_blank">Monkee-Boy Web Design</a> in Austin, TX. </p>
+      <p>I've been developing professionally for five years, most recently as a Front End Web Developer at <a href="https://monkee-boy.com" target="_blank">Monkee-Boy Web Design</a> in Austin, TX. </p>
 
-      <p>I pride myself on creating user-friendly, accessible, and responsive websites. My current techology stack includes HTML, CSS, JS (ES6, Node, and React), and PHP, but I love learning about different libraries and frameworks that can improve my work. <Link to="/portfolio">Check out my portfolio</Link> to see what I've been working on professionally and in my free time.</p>
+      <p>I pride myself on creating user-friendly, accessible, and responsive websites. My current techology stack includes HTML, CSS, JS (ES6, Node, and React), and PHP, but I love learning about different libraries and frameworks that can improve my work and create better experiences for my clients. <Link to="/portfolio">Check out my portfolio</Link> to see what I've been working on professionally and in my free time.</p>
 
-      <p>In my free time, I really enjoy hiking, biking, running, and reading.</p>
+      <p><Link to="/blog">Check out my blog</Link> if you'd like to hear my thoughts on development topics.</p>
 
-      <div className="current-section">
-        <div className="spotify">
-          <p>At the moment, I'm currently listening to: </p>
-          {/* Add Spotify currently listening */}
-        </div>
-        
-        <div className="book">
-          <p>At the moment, I'm currently reading: </p>
-          {/* Add currently reading book section */}
-        </div>
-      </div>  
+      <p>Outside of coding, I really enjoy hiking/backpacking, biking, running, and reading.</p>
     </div>
   )
 }
