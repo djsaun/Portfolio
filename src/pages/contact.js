ff1d85f79c6de7415c6cfee465afc7e65622c780
David Saunders <david@monkee-boy.com>
2018-04-06 08:15:50 -0500

    Removed contact form in lieu of email button; Still trying to figure out why the mailgun api isn't working properly

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ContactForm from '../components/ContactForm';
+import Button from '../components/Button';
 
 class Contact extends React.Component{
-  constructor() {
-    super();
-  }
-
   render() {
+
+    const socialStyle = {
+      marginTop: '40px'
+    }
+
     return(
       <div>
         <h1>Get In Touch</h1>
-        <h3>Let's start a conversation!</h3>
-        <p>I'm interested in learning more about your project. Please provide as much detail as you can below. You should expect to hear from me in a day or so.</p>
-        <ContactForm />
+        <h2>Let's start a conversation!</h2>
+        <p>I'm interested in learning more about your project. Please provide as much detail as you can in your email. You should expect to hear from me in a day or so.</p>
+        {/* <ContactForm /> */}
+        <Button link="mailto:djsaun@gmail.com?subject=Project Inquiry for David Saunders" text="Send Email" />
+
+        <p style={socialStyle}>If you'd like to connect on social media, you can find me on <a href="https://github.com/djsaun" target="_blank">GitHub</a>, <a href="https://www.linkedin.com/in/davidjsaunders/" target="_blank">LinkedIn</a>, and <a href="https://twitter.com/djsaun01" target="_blank">Twitter</a>.</p>
       </div>
     )
   }
