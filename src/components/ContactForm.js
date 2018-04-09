2e1b334c5d998a3a2fb548e7b610a714d13be85c
David Saunders <david@monkee-boy.com>
2018-04-09 08:54:51 -0500

    Form debugging

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,6 +8,12 @@ import { faMicrophone, faPhone, faDesktop } from '@fortawesome/fontawesome-free-
 import Button from '../components/Button';
 import styles from '../styles/contactForm.module.css';
 
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
 class ContactForm extends React.Component {
   constructor() {
     super();
@@ -18,199 +24,83 @@ class ContactForm extends React.Component {
     }
   }
 
-  componentDidMount() {
-    const script = document.createElement("script");
-    script.src =
-      "https://www.google.com/recaptcha/api.js";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
+  // componentDidMount() {
+  //   const script = document.createElement("script");
+  //   script.src =
+  //     "https://www.google.com/recaptcha/api.js";
+  //   script.async = true;
+  //   script.defer = true;
+  //   document.body.appendChild(script);
+  // }
+
+  // executeCaptcha = function () {
+  //   recaptchaInstance.execute();
+  // };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  executeCaptcha = function () {
-    recaptchaInstance.execute();
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
   };
 
   render() {
 
-    const self = this;
+    // const self = this;
 
-    const encode = data =>
-      Object.keys(data)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-        .join('&')
+    // const encode = data =>
+    //   Object.keys(data)
+    //     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    //     .join('&')
 
     return (
+
       <div>
-        {this.state.error ? <p className={styles.error}>An error occurred while submitting the form. Please try again. If the problem persists, please email me at <a href="mailto:djsaun@gmail.com">djsaun@gmail.com</a></p> : ''}
-        {(this.state.submitted === true && !this.state.error) ? 
-        
-        <p className={styles.confirmationMessage}>Thank you for contacting me!<br/><br/>I'm reviewing your submission and should get back to you in the next 24 hours.</p> : 
-        
-        <Formik
-          initialValues={{
-            name: '',
-            email: '',
-            phone: '',
-            website: '',
-            message: '',
-            captcha: ''
-          }}
-          validate={values => {
-
-            let errors = {};
-            if (!values.name) {
-              errors.name = 'Required';
-            }
-
-            if (!values.email) {
-              errors.email = 'Required';
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-            ) {
-              errors.email = 'Invalid email address';
-            }
-
-            if (!/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/i.test(values.phone)) {
-              errors.phone = 'Invalid phone number'
-            }
-
-            if (!values.message) {
-              errors.message = 'Required';
-            }
-
-            if (!values.captcha) {
-              errors.captcha = 'Required';
-            }
-
-            return errors;
-          }}
-          onSubmit={(
-            values,
-            { setSubmitting, setErrors /* setValues and other goodies */ }
-          ) => {
-            let emailText = '';
-
-            Object.keys(values).map((value, i) => {
-              emailText += `<p><strong>${value}</strong>: ${values[value]} </p><br/>`
-            })
-
-            axios({
-              method: 'post',
-              url: `https://api.mailgun.net/v3/${process.env.GATSBY_DOMAIN}/messages`,
-              auth: {
-                username: 'api',
-                password: process.env.GATSBY_MAILGUN_API_KEY
-              },
-              params: {
-                from: 'David Saunders <david@monkee-boy.com>',
-                to: 'djsaun@gmail.com',
-                subject: 'New Submission From Portfolio Contact Form',
-                html: emailText
-              }
-            }).then((response) => {
-               console.log(response);
-               this.setState({submitted: true})
-             })
-            .catch((error)=>{
-               console.log(error);
-              this.setState({ submitted: false, error })
-            });
-            
-          }}
-          render={({
-            values,
-            errors,
-            touched,
-            setFieldValue,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-          }) => (
-              <form onSubmit={handleSubmit} name="contact" data-netlify="true" data-netlify-honeypot="bot-field" className={styles.form}>
-                <div className={styles.field}>
-                  <label htmlFor="name"><FontAwesomeIcon className={styles.icon} icon={faUser} /> Name<span>*</span></label>
-                  <input
-                    type="text"
-                    name="name"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.name}
-                    className={(touched.name && errors.name) ? styles.hasError : ''}
-                  />
-                  {touched.name && errors.name && <div className={styles.errorText}>{errors.name}</div>}
-                </div>
-
-                <div className={styles.field}>
-                  <label htmlFor="email"><FontAwesomeIcon className={styles.icon} icon={faEnvelope} /> Email<span>*</span></label>
-                  <input
-                    type="email"
-                    name="email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    className={(touched.email && errors.email) ? styles.hasError : ''}
-                  />
-                  {touched.email && errors.email && <div className={styles.errorText}>{errors.email}</div>}
-                </div>
-
-                <div className={styles.field}>
-                  <label htmlFor="phone"><FontAwesomeIcon className={styles.icon} icon={faPhone} /> Phone Number</label>
-                  <input
-                    type="tel"
-                    name="phone"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.phone}
-                    className={(touched.phone && errors.phone) ? styles.hasError : ''}
-                  />
-                  {touched.phone && errors.phone && <div className={styles.errorText}>{errors.phone}</div>}
-                </div>
-
-                <div className={styles.field}>
-                  <label htmlFor="website"><FontAwesomeIcon className={styles.icon} icon={faDesktop} /> Website</label>
-                  <input
-                    type="text"
-                    name="website"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.website}
-                  />
-                </div>
-
-                <div className={styles.field}>
-                  <label htmlFor="message"><FontAwesomeIcon className={styles.icon} icon={faMicrophone} /> How Can I Help?<span>*</span></label>
-                  <textarea
-                    name="message"
-                    rows='5'
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.message}
-                    className={(touched.message && errors.message) ? styles.hasError : ''}
-                  ></textarea>
-                  {touched.message && errors.message && <div className={styles.errorText}>{errors.message}</div>}
-                </div>
-
-                <div className={styles.captcha}>
-                  <Recaptcha
-                    sitekey={process.env.GATSBY_CAPTCHA_KEY}
-                    render="explicit"
-                    verifyCallback={(response) => { setFieldValue("captcha", response) }}
-                    onloadCallback={() => { console.log("captcha loaded"); }}
-                  />
-                  {touched.captcha && errors.captcha && <div className={styles.errorText}>{errors.captcha}</div>}
-                </div>
-
-                <div className={styles.formButton}>
-                  <Button type="submit" text="Submit" className={styles.button}>
-                    Submit
-                  </Button>
-                </div>
-              </form>
-            )}
-        />
-      }
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:<br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </div>
     )
   }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,8 +14,8 @@ class Contact extends React.Component{
         <h1>Get In Touch</h1>
         <h2>Let's start a conversation!</h2>
         <p>I'm interested in learning more about your project. Please provide as much detail as you can in your email. You should expect to hear from me in a day or so.</p>
-        {/* <ContactForm /> */}
-        <Button link="mailto:djsaun@gmail.com?subject=Project Inquiry for David Saunders" text="Send Email" />
+        <ContactForm />
+        {/* <Button link="mailto:djsaun@gmail.com?subject=Project Inquiry for David Saunders" text="Send Email" /> */}
 
         <p style={socialStyle}>If you'd like to connect on social media, you can find me on <a href="https://github.com/djsaun" target="_blank">GitHub</a>, <a href="https://www.linkedin.com/in/davidjsaunders/" target="_blank">LinkedIn</a>, and <a href="https://twitter.com/djsaun01" target="_blank">Twitter</a>.</p>
       </div>
