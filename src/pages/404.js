11a9c59e65fa5568b6c38e0e5b3cc2a4bf113549
David Saunders <david@monkee-boy.com>
2018-04-09 07:48:56 -0500

    Updated links on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Link from 'gatsby-link';
 
 const notFound = (props) => {
   return (
     <div>
       <h1>404!</h1>
       <h3>Looks like you've stumbled across a broken link!</h3>
-      <p>No worries. Just <a href="/">click here</a> to return to the homepage.</p>
+      <p>No worries. Just <Link to="/">click here</Link> to return to the homepage.</p>
       <p>You may also <a href="mailto:djsaun@gmail.com">click here</a> to contact me.</p>
     </div>
   )
