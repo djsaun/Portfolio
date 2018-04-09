e92f8b091cf525128557bd2d56d5feabfa7765ae
David Saunders <david@monkee-boy.com>
2018-04-09 09:29:07 -0500

    Form debugging

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -32,44 +32,17 @@ export default class ContactForm extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          <p hidden>
-            <label>
-              Don’t fill this out: <input name="bot-field" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:<br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </div>
+      <form
+        name="contact-form"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input name="name" placeholder="Your Name" type="text" />
+        <input name="email" placeholder="name@name.com" type="email" />
+        <textarea name="message" />
+        <button>Send</button>
+      </form>
     );
   }
 }
